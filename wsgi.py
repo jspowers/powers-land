b8a611364746622ede07b0240e3c4097e18e7230
James Powers <47932894+jspowers@users.noreply.github.com>
2025-12-20 12:23:54 -0600

    server update

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 from app import create_app
 
 # Determine environment from FLASK_ENV or default to production
-config_name = os.environ.get('FLASK_ENV', 'development')
+config_name = os.environ.get('FLASK_ENV', 'production')
 application = create_app(config_name)
 
 if __name__ == '__main__':
